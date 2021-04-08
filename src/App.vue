8a9fd44a0df5838ac81ade0c9799346bd66e3595
shenmenglong <shenmenglong@tydic.com>
2021-04-08 18:34:35 +0800

    缓存指定页面

--- src/App.vue
+++ src/App.vue
@@ -2,9 +2,16 @@
   <div id="app">
     <div id="nav">
       <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+      <router-link to="/about">About</router-link> |
+      <router-link to="/other">Other</router-link>
     </div>
-    <router-view/>
+<!--    <keep-alive>
+      <router-view v-if="$route.meta.keepAlive"></router-view>
+    </keep-alive>
+    <router-view v-if="!$route.meta.keepAlive"></router-view>-->
+    <keep-alive include="About">
+      <router-view></router-view>
+    </keep-alive>
   </div>
 </template>
 
