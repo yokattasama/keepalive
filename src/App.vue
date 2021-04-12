1631445a896aa412f8c48d0de69f872fafdad34d
shenmenglong <shenmenglong@tydic.com>
2021-04-12 14:50:37 +0800

    添加操作说明，配置说明

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <template>
   <div id="app">
+
     <div id="nav">
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link> |
@@ -7,6 +8,8 @@
       <router-link to="/SonA">子路由A</router-link>
     </div>
     <div>{{shouldKeepAlive}}</div>
+    <p>配置说明：子路由A是 about的子路由，并在keppalive中缓存，其他路由均不缓存，about 也不缓存</p>
+    <p>操作说明：默认进入Home页面， 首先点击进入 about， 再次 点击查看子路由A， 查看控制台打印</p>
     <keep-alive>
       <router-view v-if="$route.meta.keepAlive"></router-view>
     </keep-alive>
